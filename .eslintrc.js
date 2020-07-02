181097e479211fb384cb1bc103486be2358bc80a
GibboK <gibbok.coding@gmail.com>
2020-07-02 11:53:34 +0200

    fix issue with eslint not recognized warning instead use 1

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,7 +14,7 @@ module.exports = {
   },
   rules: {
     '@typescript-eslint/explicit-function-return-type': [
-      'warning',
+      1,
       {
         allowHigherOrderFunctions: true,
       },
