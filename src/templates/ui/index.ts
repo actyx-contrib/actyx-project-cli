2e9de0ea06ec8eee6158b319c749ca90584e1151
GibboK <gibbok.coding@gmail.com>
2020-07-10 13:44:42 +0200

    fix return type

--- src/templates/ui/index.ts
+++ src/templates/ui/index.ts
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-export const defaultHtml = (appName: string) => `<!DOCTYPE html>
+export const defaultHtml = (appName: string): string => `<!DOCTYPE html>
 <html>
   <head>
     <meta charset="UTF-8">
