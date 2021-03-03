9efecbc17a305b718e7e4c748d5cc0e42402c429
Alex Halemba <alex-halemba@gmx.de>
2021-03-03 14:41:29 +0100

    reload page when actyxOS is not available

--- src/templates/ui/index.ts
+++ src/templates/ui/index.ts
@@ -88,9 +88,13 @@ import * as ReactDOM from 'react-dom'
 import { Pond } from '@actyx-contrib/react-pond'
 import { App } from './App'
 
+const onError = () => {
+  setTimeout(() => location.reload(), 2500)
+}
+
 ReactDOM.render(
   <React.StrictMode>
-    <Pond loadComponent={<div>Connecting to ActyxOS</div>}>
+    <Pond loadComponent={<div>Connecting to ActyxOS</div>} onError={onError}>
       <App />
     </Pond>
   </React.StrictMode>,
